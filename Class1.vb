8217c6ab5a6d5d401c73df9568289ba8bfd9d3e9
s.kogawa <s.kogawa@sbfw.co.jp>
2020-11-24 21:53:48 +0900

    initial commit

---
+++ Class1.vb
@@ -0,0 +1,59 @@
+﻿Imports Microsoft.Office.Interop
+Public Structure ExcelInfomation
+    Dim xlApp As Excel.Application
+    Dim xlWBs As Excel.Workbooks
+    Dim xlWB As Excel.Workbook
+End Structure
+
+Public Class ClsParent
+
+    public Function Parent() As Boolean
+
+        Dim excel As ExcelInfomation
+        Dim location As String = "D:\worl\Sample.xlsx"
+        
+        Try
+            Dim cls As New ClsChild()
+            For  i = 0 To 2
+                cls.Child(location, excel)
+
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excel.xlWB)
+                excel.xlWB = Nothing
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excel.xlWBs)
+                excel.xlWBs = Nothing
+
+                GC.Collect()
+                GC.WaitForPendingFinalizers()
+                GC.Collect()
+
+                excel.xlApp.Quit()
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excel.xlApp)
+                excel.xlApp = Nothing
+
+                GC.Collect()
+                GC.WaitForPendingFinalizers()
+                GC.Collect()
+            Next
+        Catch ex As Exception
+        Finally
+        End Try
+
+        Return True
+    End Function
+End Class
+
+Public Class ClsChild
+    public Sub Child(ByRef location As String, ByRef excel As ExcelInfomation)
+
+        Try
+            excel.xlApp = New Excel.Application()
+            excel.xlApp.DisplayAlerts = False
+            excel.xlWBs = excel.xlApp.Workbooks
+            excel.xlWB = excel.xlWBs.Open(location)
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+End Class
---
+++ Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim cls As New ClsParent()
+        If cls.Parent() Then
+            MsgBox("True")
+        End If
+    End Sub
+
+End Module
